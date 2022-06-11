4c539fc7e1c9cf6db5412b931accf0e9dd267011
Boakai <boakaidkamara@gmail.com>
2022-06-11 22:42:04 +0000

    generating random index

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 options = ["R", "P", "S"]
 
 # prompt user for to select an option 
@@ -8,6 +10,11 @@ print(user_option)
 # validate user option 
 while user_option not in options:
     user_option =  input('Please select a valid option: R,P,S\n')
+
+
+# generate random index 
+random_number = random.randint(0,2)
+print(random_number)
 
 
 
