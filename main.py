1333fcd77690534b91706c58dfda59850980f3f2
Boakai <boakaidkamara@gmail.com>
2022-06-12 10:06:57 +0000

    restarting game after score is tie

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 import random
 
 options = ["R", "P", "S"]
-
-# prompt user for to select an option 
-user_option =  input('Select an option from the list: R,P,S\n')
-print(user_option)
-
-
-# validate user option 
-while user_option not in options:
-    user_option =  input('Please select a valid option: R,P,S\n')
+def startGame():
+    # prompt user for to select an option 
+    user_selection =  input('Select an option from the list: R,P,S\n')
+    # print(user_selection)
 
 
-# generate random index 
-random_number = random.randint(0,2)
-print(random_number)
+    # validate user option 
+    while user_selection not in options:
+        user_selection =  input('Please select a valid option: R,P,S\n')
 
 
+    # generate random index 
+    random_index = random.randint(0,2)
+
+
+    # get computer selection
+    computer_selection = options[random_index]
+    print(computer_selection)
+
+
+    # check if game is tie 
+    if user_selection == computer_selection:
+        print("Tie")
+        startGame()
+    elif user_selection != computer_selection: #find and announce winner
+        
+        print("winner announcement")
+        print("value not the same")
+        if user_selection == "R" and computer_selection == "S":
+            print("Human Wins")
+        elif user_selection == "S" and computer_selection == "R":
+            print("Computer wins")
+        elif user_selection == "P" and computer_selection == "S":
+            print("Computer wins")
+        elif user_selection == "S" and computer_selection == "P":
+            print("Computer wins")
+        elif user_selection == "P" and computer_selection == "R":
+            print("Computer wins")
+        elif user_selection == "R" and computer_selection == "P":
+            print("Computer wins")
+        elif user_selection == computer_selection:
+            print("game tie")
+
+
+
+# invoke the fnx 
+startGame()
 
 
 
