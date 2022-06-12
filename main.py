6c746dc18f413c68aa0edc189a74f579b8fcb6d3
Boakai <boakaidkamara@gmail.com>
2022-06-12 10:29:58 +0000

    printing player options

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ options = ["R", "P", "S"]
 def startGame():
     # prompt user for to select an option 
     user_selection =  input('Select an option from the list: R,P,S\n')
-    # print(user_selection)
 
 
     # validate user option 
     while user_selection not in options:
         user_selection =  input('Please select a valid option: R,P,S\n')
+    
 
 
     # generate random index 
@@ -18,17 +18,34 @@ def startGame():
 
     # get computer selection
     computer_selection = options[random_index]
-    print(computer_selection)
+
+
+
+    # print player selection
+    # HUMAN 
+    if user_selection == "R":
+        print("Player (Rock)")
+    elif user_selection == "P":
+            print("Player (Paper)")
+    elif user_selection == "S":
+        print("Player (Scsissors)")
+
+    # COMPUTER 
+    if computer_selection == "R":
+        print("CPU (Rock)")
+    elif computer_selection == "P":
+            print("CPU (Paper)")
+    elif computer_selection == "S":
+        print("CPU (Scsissors)")
+
 
 
     # check if game is tie 
     if user_selection == computer_selection:
         print("Tie")
-        startGame()
-    elif user_selection != computer_selection: #find and announce winner
-        
-        print("winner announcement")
-        print("value not the same")
+        startGame() # restart the game after game is tie 
+    elif user_selection != computer_selection:
+        #find and announce winner the winner after the is not tie
         if user_selection == "R" and computer_selection == "S":
             print("Human Wins")
         elif user_selection == "S" and computer_selection == "R":
